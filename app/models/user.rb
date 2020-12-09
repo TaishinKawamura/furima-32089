91649c5c049f2f67d5ce8c6f1ef343b86e6761bd
TaishinKawamura <68317062+TaishinKawamura@users.noreply.github.com>
2020-12-09 09:53:58 +0900

    validates追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :birthday, presence: true
+  validates :email, {presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }}
+  validates :family_name, :first_name, {presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }}
+  validates :family_kana, :first_kana, {presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }}
+  validates :password, {presence: true, length: { minimum: 6 }}
+
 end
