de919ebd13cc251f0b8817866bc359f232da0881
TaishinKawamura <68317062+TaishinKawamura@users.noreply.github.com>
2020-12-16 19:09:24 +0900

    rubocopの自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :birthday, presence: true
-  #validates :email, {presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }}
-  validates :family_name, :first_name, {presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }}
-  validates :family_kana, :first_kana, {presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }}
-  validates :password, {presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }}
-
+  # validates :email, {presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }}
+  validates :family_name, :first_name, { presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } }
+  validates :family_kana, :first_kana, { presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } }
+  validates :password, { presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i } }
 end
